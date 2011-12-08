97e5d63a8b556eaab1780c41fa7c04a9de085947
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-12-07 21:41:08 -0800

    First commit, raw Rails skeleton, w/o ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AresTalk::Application.config.secret_token = '90b144f70c3cd0614d706c809dd366e6a5decc11684abba0a0acec7c4333dc38209ed95b3fdc4c0c7aa71269e2610ba47e3608ad3c1f6e722078ec190024232d'
